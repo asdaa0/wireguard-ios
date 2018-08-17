937635e69840a04ec5835d771f39c31928a7e8de
Roopesh Chander <roop@roopc.net>
2018-08-18 01:49:53 +0530

    Try connecting to demo.wireguard.com

--- WireGuard/WireGuard/MainViewController.swift
+++ WireGuard/WireGuard/MainViewController.swift
@@ -35,14 +35,22 @@ class MainViewController: UIViewController {
         if (tunnelController.isTunnelConnected) {
             tunnelController.stopTunnel()
         } else {
+            let demoServerSettings = """
+            private_key=98add04f289e76a2501c44926f4cfc761620de0baf5ff12d7c70b07ea2f3286d
+            replace_peers=true
+            public_key=25123c5dcd3328ff645e4f2a3fce0d754400d3887a0cb7c56f0267e20fbf3c5b
+            endpoint=163.172.161.0:12912
+            replace_allowed_ips=true
+            allowed_ip=0.0.0.0/0
+            """
             let interface = TunnelController.TunnelInterface(
                 name: "wg0.conf",
-                localizedDescription: "Test",
-                settings: "settings_test",
-                serverAddress: "server_address_test",
-                address: "address_test",
-                subnetMask: "subnet_mask_test",
-                dnsServers: ["dns_test"]
+                localizedDescription: "Demo server",
+                settings: demoServerSettings,
+                serverAddress: "demo.wireguard.com",
+                address: "192.168.4.207",
+                subnetMask: "255.255.255.0",
+                dnsServers: ["8.8.8.8", "8.8.4.4", "1.1.1.1", "1.0.0.1"]
             )
             tunnelController.startTunnel(interface: interface)
             tunnelController.delegate = self
